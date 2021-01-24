2423fc91ec8a4728433e21a8729d65d0599233c3
UlugbekMuslitdinov <ulugbekmuslitdinov@gmail.com>
2021-01-24 20:03:53 +0500

    props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+
+function Food({name}){
+  return (<h3>I love eating {name}</h3>);
+}
+
 function App() {
   return (
     <div>
       Priveе
+      <Food name="Borsche" />
+      <Food name="Soup" />
+      <Food name="Beef" />
     </div>
   );
 }
