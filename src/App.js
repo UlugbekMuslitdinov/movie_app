b746c3a02c160693fb93cf6613a9d09c16648bc0
UlugbekMuslitdinov <ulugbekmuslitdinov@gmail.com>
2021-01-24 20:21:47 +0500

    maps

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 
-function Food({name}){
-  return (<h3>I love eating {name}</h3>);
+function Food({name, picture}){
+  return (
+    <div>
+      <h3>I love {name}</h3>
+      <img src={picture} alt=""/>
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    'name': 'Borsche',
+    'image': 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fprimebeef.ru%2Fimages%2Fcms%2Fdata%2F3-501.jpg&f=1&nofb=1',
+  },
+]
 
 function App() {
   return (
     <div>
-      Priveе
-      <Food name="Borsche" />
-      <Food name="Soup" />
-      <Food name="Beef" />
+      {foodILike.map(dish => <Food name={dish.name} picture={dish.image} />)}
     </div>
   );
 }
