86407e6871c3e40121d250001e89627bd8a6ddbc
UlugbekMuslitdinov <ulugbekmuslitdinov@gmail.com>
2021-01-28 21:54:01 +0500

    deploy changes

--- src/Movie.js
+++ src/Movie.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({ id, year, title, summary, poster, genres }) {
+function Movie({ year, title, summary, poster, genres }) {
   return (
     <div className="movie">
       <img src={poster} alt={title} title={title} />
